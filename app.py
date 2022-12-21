422150e6b78cfce31f29f8709f07ec1d7b9a8862
MuradAsadzade <muradesedzade2004@gmail.com>
2022-12-21 20:52:21 +0400

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+
+# Task1
+# a='5382-1739-9201-9017'
+# for i in range(0,len(a)):
+#     if i<9:
+#         print("*",end=" ")
+#     else:
+#         print(a[i],end=" ")
+
+
+
+#Task2
+# a=15
+# b=4
+# c=a%b
+# d=c**3
+# print(d)
+
+# Task3
+# a=256.91872
+# # print("{:.2f}".format(a))
+# print("{:2f}".format(a))
+
+# Task4
+# a='34'
+# width=5
+# print(a.rjust(width,'0'))
+
+# Task5
+# a=input("Eded:")
+# uzun=0
+# for i in range(0,len(a)):
+#     if a[i]=='.':
+#         break
+#     else:
+#         uzun+=1
+# print(uzun)
+
+# Task6
+# a="https://instagram  .com"
+# print(a.strip('https://.com'))
+
+# Task7
+# cumle=input("Cumle:")
+# kohne=input("Kohne soz:")
+# yeni=input("Yeni soz:")
+
+# print(cumle.replace(kohne,yeni))
